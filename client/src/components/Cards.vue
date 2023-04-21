3c0df2fd5418fd68d0ff7bdac039248281106a3a
seancampbell3161 <sean.campbell3161@gmail.com>
2023-04-20 22:56:21 -0500

    restructure

---
+++ client/src/components/Cards.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <ul>
+            <li v-for="card in cards">{{ card }}</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        mounted() {
+            console.log('mounted');
+        },
+        data() {
+            return {
+                cards: ['air','fire','water','earth']
+            }
+        }
+    }
+</script>
+
+
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import Cards from '../components/Cards.vue'
+  components: {
+    Cards
+  }
+
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <Cards />
+  </main>
+</template>
